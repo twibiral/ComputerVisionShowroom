aff05e93aca142ad668287cb86264419e0a69eee
Tim Wibiral <tim.wibiral.tw@gmail.com>
2022-09-11 16:53:06 +0200

    feat: add website

--- src/Config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-SOURCE_DIR = Path(os.path.realpath(__file__)).parent
-TEMPLATE_DIR = SOURCE_DIR / "templates"
-
-STATIC_DIR = SOURCE_DIR.parent / "static"
-IMAGES_DIR = STATIC_DIR / "images"
